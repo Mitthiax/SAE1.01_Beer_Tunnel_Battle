41bf51480bcadf70f121ac632bb5a60a9953a590
Lucas <87207734+Lucasbmmn@users.noreply.github.com>
2024-11-27 10:13:59 +0100

    creerpartie

---
+++ unitCreerPartieIHM.pas
@@ -0,0 +1,27 @@
+unit unitCreerPartieIHM;
+
+{$mode objfpc}{$H+}
+
+interface
+  
+  // Procedure qui affiche l'interface de création d'une nouvelle partie
+  procedure creerPartieIHM(var nom:string; var taille:integer; var genre:string);
+  
+implementation
+  uses
+    GestionEcran, unitCoffreLogic;
+  
+  // Procedure qui affiche l'interface de création d'une nouvelle partie
+  procedure creerPartieIHM(var nom:string; var taille:integer; var genre:string);
+  begin
+    dessinerCadreXY(1, 0, 198, 39, simple, White, Black);
+    dessinerCadreXY(80, 0, 120, 2, simple, White, Black);
+    dessinerCadreXY(90, 1, 110, 3, simple, White, Black);
+    deplacerCurseurXY(93, 2); write('Nouvelle partie');
+
+    deplacerCurseurXY(70, 10); write('Quel est votre nom ? '); readln(nom);
+    deplacerCurseurXY(70, 12); write('Quel est votre taille ? (en cm) '); readln(nom);
+    deplacerCurseurXY(70, 14); write('Quel est votre Genre ? '); readln(genre);
+  end;
+  
+end.
