2e15e1aaf33361b8f2be73e8ac61eb101f78dcf1
yaya989 <161058028+yaya989@users.noreply.github.com>
2024-12-02 15:02:56 +0100

    Update unitTaverneIHM.pas

--- unitTaverneIHM.pas
+++ unitTaverneIHM.pas
@@ -1,54 +1,69 @@
-unit unitTaverne;
+unit unitTaverneIHM;
 
 {$codepage utf8}
 {$mode objfpc}{$H+}
 
 interface
-//Permet de savoir quel choix est fait
-procedure gererChoixTaverne(choix : Integer);
-//Affichage du journal du joueur avec tout les informations sur sont personnage
-procedure afficherJournal(); 
-//Permet d'aller boire une biere
-procedure boireBiere();
-//Permet d'aller manger
-procedure manger();
-//Permet de quittez la taverne
-procedure quittez();
-
-implementation
-uses
-  SysUtils, Classes, unitBeersIhm, unitTaverneAnecdote, unitTaverneAnecdoteIHM,gestionbuff,unitTaverneIHM, unitTaverneManger, unitTaverneMangerIhm;
-
-
-//Procedure qui gere les choix fait par le joueur a la taverne
-procedure gererChoixTaverne(choix : Integer);
-begin
-  case choix of 
-    1 : boireBiere();
-    2 : manger();
-    3 : quittez();
-  end;
-end;
-
-procedure boireBiere();
-begin
-  afficherInterfaceBiere(); // Appel de l'interface de consommation biere
-end;
-
-procedure manger();
-begin
-  afficherInterfaceManger(); //Appel de l'interface de consommation de nourriture
-end;
-
-procedure quittez();
-begin
-  afficherInterface(); // Appel de l'interface du hall pour revenir au hall
-end;
-
-//Procedure qui affiche le journal du joueur avec tout les informations sur sont personnage
-procedure afficherJournal();
-begin
-  //afficherJournalIHM();
-end;
+//Procèdure que affiche l'interface de la taverne
+procedure afficherInterfaceTaverne(); 
   
+  
+implementation
+
+uses
+  SysUtils, Classes, GestionEcran, unitTaverne,journalihm,GestionPerso,unitBeersIhm, unitTaverneAnecdoteIHM, unitTaverneMangerIhm;
+
+//Affichague du titre et du texte pincipal de la taverne
+procedure afficherTextePrincipalTaverne();
+begin
+  //En-tete
+  dessinerCadreXY(1, 1, 199, 39, simple, LightGreen, Black);
+  dessinerCadreXY(39, 1, 80, 1, double, Green, Black);
+  couleurTexte(White);
+  deplacerCurseurXY(46, 1); write(' La Taverne Patte de Cochon ');
+
+  //Texte du hall
+  couleurTexte(White);
+  deplacerCurseurXY(7,4); write('Vous entrez dans la taverne, des que vous pousser la porte vous pouvez sentir la douce odeur d''alcool qui');
+  deplacerCurseurXY(5, 5); Write('s''en émane, vous entendez des rires et bruit de chop qui se pose sur les tables de la taverne.');
+  deplacerCurseurXY(5, 8); write('Vous vous diriger vers le comptoir, vous constater que le border collie du tavernier est allongé devant la');
+  deplacerCurseurXY(7, 9); Write('cheminer sur son collier on peut lire falladar.');
+  couleurTexte(LightMagenta);
+  deplacerCurseurXY(7, 12); write('Bonjour mon Gaillard ! Bienvenue à la Patte de Cochon, mon nom est Jimmy, ici on a de la bière pour les');
+  deplacerCurseurXY(5, 13); Write('assoiffés et de la nourriture pour les affamés HaHa.');
+  deplacerCurseurXY(7, 16); write('Que puis-je pour vous mon brave ?');
+end;
+
+
+//Procedure qui affiche le choix des actions
+procedure afficherOptionsTaverne;
+begin
+  dessinerCadreXY(3, 25, 115, 36, double, LightGreen, Black);
+  couleurTexte(White);
+  deplacerCurseurXY(7, 27); Write('Que souhaitez-vous faire ?');
+
+  deplacerCurseurXY(9, 30); Write('1/ Boire une Biere Fraiche');
+  deplacerCurseurXY(9, 31); Write('2/ Casser la croute');
+  deplacerCurseurXY(9, 32); Write('3/ Quittez la Taverne (Ps : Reviens vite !!)');
+  dessinerCadreXY(100, 32, 110, 34, double, Red, Black);
+  deplacerCurseurXY(105, 33);
+end;
+
+
+// Procédure qui affiche toute l'interface de la taverne
+procedure afficherInterfaceTaverne();
+var
+  choix: Integer;
+begin
+  changerTailleConsole(200,40);
+  afficherTextePrincipalTaverne();
+  journal();
+  afficherOptionsTaverne();
+
+  // Capture et traitement du choix de l'utilisateur
+  readln(choix);
+  if (choix = 1) then afficherInterfaceBiere()
+  else if (choix = 2) then afficherInterfaceManger()
+  else if (choix = 3) then afficherInterface();
+end;
 end.
