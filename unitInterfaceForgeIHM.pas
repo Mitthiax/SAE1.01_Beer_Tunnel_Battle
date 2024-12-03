7feccc226966d37535c059dc31cde24ba7e03f0e
yaya989 <161058028+yaya989@users.noreply.github.com>
2024-12-03 21:15:54 +0100

    com

--- unitInterfaceForgeIHM.pas
+++ unitInterfaceForgeIHM.pas
@@ -1,5 +1,5 @@
 unit unitInterfaceForgeIHM;
-{Role: Permet d'afficher l'interface de la forge qui permet de forger des objets ou de quittez la forge.}
+{Role: Permet d'afficher l'interface de la forge qui permet d'acceder au forgeront ou de quittez la forge.}
 {$codepage utf8}
 {$mode objfpc}{$H+}
 
