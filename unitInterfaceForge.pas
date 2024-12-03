9d601bb1a7d842ddf8e40e8e2e93a22abaab8160
yaya989 <161058028+yaya989@users.noreply.github.com>
2024-12-03 21:10:59 +0100

    function com

--- unitInterfaceForge.pas
+++ unitInterfaceForge.pas
@@ -1,13 +1,11 @@
 unit unitInterfaceForge;
-
+{Role: Permet de forger des objets pour un prix et un certain nombres de matériaux définie.}
 {$codepage utf8}
 {$mode objfpc}{$H+}
 
 interface
 //Permet de savoir quel choix est fait
-procedure gererChoixForge(choix : Integer);
-//Affichage du journal du joueur avec tout les informations sur sont personnage
-procedure afficherJournal(); 
+function gererChoixForge(choix : Integer):Integer;
 //Permet d'aller forger
 procedure forger();
 //Permet de quittez la forge
@@ -19,11 +17,11 @@ uses
 
 
 //Procedure qui gere les choix fait par le joueur a la forge
-procedure gererChoixForge(choix : Integer);
+function gererChoixForge(choix : Integer):Integer;
 begin
   case choix of 
-    1 : forger();
-    2 : quittez();
+    1 : forger(); //Allez forger
+    2 : quittez(); //Retournez au hall
   end;
 end;
 
@@ -36,12 +34,5 @@ end;
 procedure quittez();
 begin
   afficherInterface(); // Appel de l'interface du hall pour revenir au hall
-end;
-
-//Procedure qui affiche le journal du joueur avec tout les informations sur sont personnage
-procedure afficherJournal();
-begin
-  //afficherJournalIHM();
-end;
-  
+end; 
 end.
